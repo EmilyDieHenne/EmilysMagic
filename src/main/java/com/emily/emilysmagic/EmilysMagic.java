dddecc9d63279b9b1149145b87e31be898189592
Falk Abschlag <falkabschlag@gmx.de>
2023-04-10 19:31:09 +0200

    Initial commit

---
+++ src/main/java/com/emily/emilysmagic/EmilysMagic.java
@@ -0,0 +1,46 @@
+package com.emily.emilysmagic;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.CreativeModeTabEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(EmilysMagic.MOD_ID)
+public class EmilysMagic
+{
+    public static final String MOD_ID = "emilysmagic";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public EmilysMagic()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::commonSetup);
+
+        //BLOCKS
+        //ITEMS
+
+        MinecraftForge.EVENT_BUS.register(this);
+
+        // Register the item to a creative tab
+        modEventBus.addListener(this::addCreative);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+
+    }
+
+    private void addCreative(CreativeModeTabEvent.BuildContents event)
+    {
+        if (event.getTab() == CreativeModeTabs.BUILDING_BLOCKS){
+
+        }
+
+    }
+
+}
