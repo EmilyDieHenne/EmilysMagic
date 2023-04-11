91ef11b567ee6937dc5aa302b25801c2db8133c3
Falk Abschlag <falkabschlag@gmx.de>
2023-04-11 15:27:40 +0200

    better poison magic

--- src/main/java/com/emily/emilysmagic/item/custom/ItemPoisonWand.java
+++ src/main/java/com/emily/emilysmagic/item/custom/ItemPoisonWand.java
@@ -1,15 +1,19 @@
 package com.emily.emilysmagic.item.custom;
 
 import com.emily.emilysmagic.projectile.FireMagic;
-import com.emily.emilysmagic.projectile.PoisonMagic;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.AreaEffectCloud;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.ThrownPotion;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.alchemy.Potion;
+import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.Vec3;
 
 
@@ -23,20 +27,20 @@ public class ItemPoisonWand extends Item {
 
 
         if (!world.isClientSide() ){
+            BlockHitResult block = getPlayerPOVHitResult(world, player, ClipContext.Fluid.NONE);
 
-            shootArrow(world, player, player.getLookAngle());
+            AreaEffectCloud cloud = new AreaEffectCloud(EntityType.AREA_EFFECT_CLOUD, world);
+            cloud.setPos(block.getBlockPos().getCenter());
+            cloud.setPotion(Potion.byName("poison"));
+
+            cloud.setDuration(50);
+            world.addFreshEntity(cloud);
+
             player.getCooldowns().addCooldown(this, 60);
         }
         player.playSound(SoundEvents.WITCH_CELEBRATE);
         return super.use( world, player, hand);
 
     }
-    private void shootArrow(Level world, Player player, Vec3 direction){
 
-        PoisonMagic arrow = new PoisonMagic(world,player);
-        arrow.setDeltaMovement(direction);
-        arrow.setSilent(true);
-        arrow.onAddedToWorld();
-        player.level.addFreshEntity(arrow);
-    }
 }
